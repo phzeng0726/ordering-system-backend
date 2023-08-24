14cdf20b7246252e157b952d7349406557bd62ba
Zeng, Rita <rita.zeng@acer.com>
2023-08-24 11:39:58 +0800

    chore: switch create menu to gorm

--- database/manager.go
+++ database/manager.go
@@ -12,7 +12,7 @@ import (
 func Connect() *gorm.DB {
 	appConfig := config.Env
 	// 設定資料庫連線字串
-	dsn := fmt.Sprintf("%s:%s@/%s", appConfig.UserName, appConfig.Password, appConfig.DBName)
+	dsn := fmt.Sprintf("%s:%s@/%s?parseTime=true", appConfig.UserName, appConfig.Password, appConfig.DBName)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
