6591819dc1075d13ebdcde389bcd259b4b79eee7
Zeng, Rita <rita.zeng@acer.com>
2023-08-24 09:48:42 +0800

    refactor: clean store gorm dirty code

--- database/manager.go
+++ database/manager.go
@@ -20,35 +20,6 @@ func Connect() *gorm.DB {
 		log.Fatal(err)
 	}
 
-	// err = db.Ping()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// db.SetConnMaxLifetime(time.Duration(10) * time.Second)
-	// db.SetMaxIdleConns(5)
-	// db.SetMaxOpenConns(2)
-
 	fmt.Println("Connected to MySQL database!")
 	return db
 }
-
-// func Open() (*Manager, error) {
-// 	appConfig := config.Env
-
-// 	// 設定資料庫連線字串
-// 	dsn := fmt.Sprintf("%s:%s@/%s", appConfig.UserName, appConfig.Password, appConfig.DBName)
-// 	db, err := sql.Open("mysql", dsn)
-
-// 	db.SetConnMaxLifetime(time.Duration(10) * time.Second)
-// 	db.SetMaxIdleConns(5)
-// 	db.SetMaxOpenConns(2)
-// 	return &Manager{db}, err
-// }
-
-// func (m *Manager) Close() error {
-// 	if m.db != nil {
-// 		return m.db.Close()
-// 	}
-// 	return nil
-// }
